d8f6d09554b48c46118bfe8f395905958cbbfbf6
Yuhei Yasuda <yuhei.yasuda1003@gmail.com>
2018-06-10 18:35:37 +0900

    init

---
+++ README.md
@@ -0,0 +1,26 @@
+# &lt;animated-collapse&gt; element
+
+&lt;animated-collapse&gt; element makes it easy and simple to collapse wrapped elements with animation.
+
+This element is built with Web Components. It works only with browsers supporting Custom Elements and Shadow DOM.
+
+## Install
+
+```
+yarn add @yuheiy/animated-collapse-element
+```
+
+## Usage
+
+```js
+import AnimatedCollapse from '@yuheiy/animated-collapse-element'
+customElements.define(AnimatedCollapse.is, AnimatedCollapse)
+```
+
+```html
+<animated-collapse>
+  <p>Place your content here!</p>
+</animated-collapse>
+```
+
+[See for details](https://yuheiy.github.io/animated-collapse-element/)
---
+++ animated-collapse-element.js
@@ -0,0 +1,126 @@
+const DEFAULT_DURATION_EXPAND = '0.25s'
+const DEFAULT_DURATION_COLLAPSE = '0.2s'
+const DEFAULT_EASING_EXPAND = 'cubic-bezier(0.4, 0, 0.2, 1)'
+const DEFAULT_EASING_COLLAPSE = 'cubic-bezier(0.4, 0, 0.2, 1)'
+
+export default class AnimatedCollapse extends HTMLElement {
+  static get observedAttributes() {
+    return ['expanded']
+  }
+
+  static get is() {
+    return 'animated-collapse'
+  }
+
+  set expanded(val) {
+    if (val) {
+      this.setAttribute('expanded', '')
+    } else {
+      this.removeAttribute('expanded')
+    }
+  }
+
+  get expanded() {
+    return this.hasAttribute('expanded')
+  }
+
+  constructor() {
+    super()
+    this.attachShadow({
+      mode: 'open',
+    }).innerHTML =
+      '<style>:host{display: block}:host([hidden]){display: none}</style>' +
+      '<div id="wrapper"><div id="container"><slot></slot></div></div>'
+
+    this._state = this.expanded ? 'expanded' : 'collapsed' // 'expanding' | 'expanded' | 'collapsing' | 'collapsed'
+    this._transitionCallback = null
+  }
+
+  connectedCallback() {
+    const wrapperEl = this.shadowRoot.querySelector('#wrapper')
+    const wrapperStyle = wrapperEl.style
+
+    if (!this.expanded) {
+      wrapperStyle.maxHeight = '0px'
+      wrapperStyle.visibility = 'hidden'
+      wrapperEl.setAttribute('aria-hidden', 'true')
+    }
+
+    wrapperEl.addEventListener(
+      'transitionend',
+      this._onTransitionEnd.bind(this),
+    )
+  }
+
+  attributeChangedCallback() {
+    if (this.expanded) {
+      this._expand()
+    } else {
+      this._collapse()
+    }
+  }
+
+  _expand() {
+    if (['expanding', 'expanded'].includes(this._state)) {
+      return
+    }
+
+    const wrapperEl = this.shadowRoot.querySelector('#wrapper')
+    const containerEl = this.shadowRoot.querySelector('#container')
+    const wrapperStyle = wrapperEl.style
+    wrapperStyle.maxHeight = `${containerEl.offsetHeight}px`
+    wrapperStyle.overflow = 'hidden'
+    wrapperStyle.visibility = ''
+    wrapperStyle.transition =
+      'max-height' +
+      ` var(--animated-collapse-duration-expand, ${DEFAULT_DURATION_EXPAND})` +
+      ` var(--animated-collapse-easing-expand, ${DEFAULT_EASING_EXPAND})`
+    wrapperEl.removeAttribute('aria-hidden')
+    this._state = 'expanding'
+    this.dispatchEvent(new CustomEvent('expandstart'))
+
+    this._transitionCallback = () => {
+      wrapperStyle.maxHeight = ''
+      wrapperStyle.overflow = ''
+      wrapperStyle.transition = ''
+      this._state = 'expanded'
+      this.dispatchEvent(new CustomEvent('expandend'))
+    }
+  }
+
+  _collapse() {
+    if (['collapsing', 'collapsed'].includes(this._state)) {
+      return
+    }
+
+    const wrapperEl = this.shadowRoot.querySelector('#wrapper')
+    const containerEl = this.shadowRoot.querySelector('#container')
+    const wrapperStyle = wrapperEl.style
+    wrapperStyle.maxHeight = `${containerEl.offsetHeight}px`
+    wrapperStyle.overflow = 'hidden'
+    wrapperStyle.transition =
+      'max-height' +
+      ` var(--animated-collapse-duration-collapse, ${DEFAULT_DURATION_COLLAPSE})` +
+      ` var(--animated-collapse-easing-collapse, ${DEFAULT_EASING_COLLAPSE})`
+    containerEl.offsetHeight // force layout
+    wrapperStyle.maxHeight = '0px'
+    wrapperEl.setAttribute('aria-hidden', 'true')
+    this._state = 'collapsing'
+    this.dispatchEvent(new CustomEvent('collapsestart'))
+
+    this._transitionCallback = () => {
+      wrapperStyle.overflow = ''
+      wrapperStyle.visibility = 'hidden'
+      wrapperStyle.transition = ''
+      this._state = 'collapsed'
+      this.dispatchEvent(new CustomEvent('collapseend'))
+    }
+  }
+
+  _onTransitionEnd() {
+    if (this._transitionCallback) {
+      this._transitionCallback()
+      this._transitionCallback = null
+    }
+  }
+}
